b361750445f758409e48fb1b8e61b04a29d4e106
Jerome Wei <jeromew@berkeley.edu>
2022-06-08 22:27:59 -0700

    refactoring and tests

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # NSYNC: Notes synchronization
 
-A single file script with minimal dependencies that produces boilerplate for daily notes '
-into custom file formats.
+A single file script with minimal dependencies that produces boilerplate for daily notes into custom file formats.
 
 ## Usage
 
-Your working directory for note-taking must contain a file named `Nsyncfile`, which contains configuration
-settings for the script.
+Your working directory for note-taking must contain a file named `Nsyncfile`, which contains configuration settings for the script.
 
 ## Requirements
 
@@ -14,11 +12,11 @@ Python 3, pandoc
 
 ## Features
 
+Creates a new boilerplate markdown file for each day.
+Option to create nested directory hierarchy.
+
 ## Philosophy
 
 Lean: we want minimal dependencies and ease of installation.
 
-Simple: configure the script with a single file, which also ensures that you are calling the 
-script in the correct working directory. Avoid complicated command line arguments in favor of the config file.
-
-
+Simple: configure the script with a single file, which also ensures that you are calling the script in the correct working directory. Avoid complicated command line arguments in favor of the config file.
--- nsync.py
+++ nsync.py
@@ -5,10 +5,9 @@ import sys
 import datetime
 
 class Settings:
-
     def __init__(self):
         relative_path = "./Nsyncfile"
-        self.markdown_header_template = """# Jerome's Daily Log, {}
+        self.header_template = """# Jerome's Daily Log, {}
 
 ## Goals for today
 
@@ -17,7 +16,6 @@ class Settings:
 ## Deadlines and Deliverables
 
 """
-
 
         self.cute_date_format = lambda d: "{0} {d.day}, {d.year}".format(d.strftime("%B"), d=d)
         self.filename_date_format = lambda d: "{d.month}-{d.day}-{d.year}".format(d=d)
@@ -38,6 +36,9 @@ def ls():
 def mkdir(dir_path):
     os.mkdir(dir_path)
 
+def pwd():
+    return os.getcwd()
+
 def cd(dir_path):
     print("Changing working directory to `{}`".format(dir_path))
     os.chdir(dir_path)
@@ -46,12 +47,16 @@ def shell_cmd(command):
     print("Executing shell command `{}`".format(command))
     os.system(command)
 
+def apply_file_extension(base_name, extension):
+    return "{}.{}".format(base_name, extension)
+
 if __name__ == "__main__":
     print("Synchronizing journal...")
     settings = Settings()
     if not settings.ok:
         print("Error: {}".format(settings.error))
         sys.exit()
+
     current_datetime = datetime.datetime.now()
     today_dir_path = settings.filename_date_format(current_datetime)
     if directory_exists(today_dir_path):
@@ -59,15 +64,16 @@ if __name__ == "__main__":
     else:
         print("Creating today's directory.")
         mkdir(today_dir_path)
-        with open(os.path.join(today_dir_path, today_dir_path + ".md"), "w") as f:
-            f.write(settings.markdown_header_template.format(settings.cute_date_format(current_datetime)))
+        with open(os.path.join(today_dir_path, apply_file_extension(today_dir_path, "md")), "w") as f:
+            f.write(settings.header_template.format(settings.cute_date_format(current_datetime)))
+
     print("Converting Markdown to PDF.")
-    calling_dir = os.getcwd()
+    calling_dir = pwd()
     for elem in ls():
         if directory_exists(elem):
             today_dir_path = elem
             cd(today_dir_path)
-            shell_cmd("pandoc {} -o {}".format(today_dir_path + ".md", today_dir_path + ".pdf"))
+            shell_cmd("pandoc {} -o {}".format(apply_file_extension(today_dir_path, "md"), apply_file_extension(today_dir_path, "pdf")))
             cd(calling_dir)
     print("Done.")
 
